e7ab053a79ebef59b8df7639626b4e2ec7b9076f
Lindsey Meyer <lameyer@users.noreply.github.com>
2017-06-19 13:27:37 -0700

    updates dist files from merge

--- dist/select.js
+++ dist/select.js
@@ -1,7 +1,7 @@
 /*!
  * ui-select
  * http://github.com/angular-ui/ui-select
- * Version: 0.19.6 - 2017-06-17T00:23:34.374Z
+ * Version: 0.19.6 - 2017-06-19T20:27:01.227Z
  * License: MIT
  */
 
@@ -1029,7 +1029,9 @@ uis.controller('uiSelectCtrl',
   });
 
   // Allow tagging on blur
-  ctrl.searchInput.on('blur', function() {
+  ctrl.searchInput.on('blur', function($event) {
+    // do not tag on blur if focus is going to element within ui-select
+    if (ctrl.$element.has($event.relatedTarget).length) return;
     if (ctrl.tagging.isActivated && ctrl.tagOnBlur) {
       $timeout(function() {
         ctrl.searchInput.triggerHandler('tagged');
